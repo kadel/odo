b07c21f210c59b0081b05a2bd835e825cd331911
Tomas Kral <tkral@redhat.com>
2019-02-28 14:38:23 +0100

    test

--- tests/e2e/link_test.go
+++ tests/e2e/link_test.go
@@ -1,6 +1,7 @@
 package e2e
 
 import (
+	"fmt"
 	"strings"
 
 	. "github.com/onsi/ginkgo"
@@ -64,6 +65,8 @@ var _ = Describe("odoLinkE2e", func() {
 		It("should be able to create a service", func() {
 			runCmdShouldPass("odo service create mysql-persistent")
 
+			out := runCmdShouldPass("oc get serviceinstance -o name")
+			fmt.Println(out)
 			waitForCmdOut("oc get serviceinstance -o name", 1, true, func(output string) bool {
 				return strings.Contains(output, "mysql-persistent")
 			})
